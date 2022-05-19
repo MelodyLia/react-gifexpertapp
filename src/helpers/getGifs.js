cfeaff1ec1dd53c5cf48532db87760a3d7f87d20
Mery Acevedo <merlianaac@gmail.com>
2022-05-19 11:21:31 -0500

    First Commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+export const getGifs = async(category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=U8HI3OA9B85Z3pZ3TrUjTBzB3AftZsnr&q=${encodeURI(category)}&limit=20`
+    const resp = await fetch(url);
+    const {data } = await resp.json();
+  
+    const gifs = data.map( img =>{
+       return{
+         id:img.id,
+         title:img.title,
+         url:img.images?.downsized_medium.url
+       }
+    }
+  
+  
+    )
+  
+    return gifs;
+   }
+
